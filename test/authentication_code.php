2c2ae2bbfc60c58a736d5592635eeb46224e9f35
rmartineza <rmartineza@vocento.com>
2016-04-19 12:48:30 +0200

    actulizado el test de authentication_code.php

--- test/authentication_code.php
+++ test/authentication_code.php
@@ -1,14 +1,6 @@
 <?php
 //show code
 //highlight_file("/info/authentication.php");
-?>
-<br />
-<form action="#"><label>CODE</label><input type="text" name="code"/><input type="submit" value="validate"/></form>
-<br/>
-<a href="https://www.nimblepayments.com/auth/connect?client_id=BD418DCC1C271E2889EBEF9AA78C561D&response_type=code">Get code here</a>
-------------------------------------------------------------------------------------------------------------
-<br />
-<?php
 require_once '../lib/Nimble/base/NimbleAPI.php';
 require_once 'functions.php';
 
@@ -16,7 +8,16 @@ $params = array(
         'clientId' => CLIENT_ID,
         'clientSecret' => CLIENT_SECRET
 );
+$NimbleApi = new NimbleAPI($params);
+?>
+<br />
+<form action="#"><label>CODE</label><input type="text" name="code"/><input type="submit" value="validate"/></form>
+<br/>
 
+<a href="<?php echo $NimbleApi->getOauth3Url();?>">Get code here</a>
+------------------------------------------------------------------------------------------------------------
+<br />
+<?php
 
 if ($_REQUEST['code']):
     $params['oauth_code'] = $_REQUEST['code'];
@@ -38,7 +39,6 @@ if ($_REQUEST['code']):
     
     //$response2 = NimbleAPIAuth::refreshToken($NimbleApi2);
     ?>
-    <hr>
     Response:
     <?php
     var_dump($NimbleApi2->authorization->getAccessToken());
