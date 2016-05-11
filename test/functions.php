7a73b85bb57668399f13bb3069885871c1497a63
rmartineza <rmartineza@vocento.com>
2016-05-11 15:17:56 +0200

    cambiado el nombre para el token y quitado el base64

--- test/functions.php
+++ test/functions.php
@@ -6,8 +6,8 @@ session_start();
 if (file_exists('gateway_config.php')) {
     include 'gateway_config.php';
     
-    if(isset($_REQUEST["session_token"])){
-       $_SESSION["session_token"] = $_REQUEST["session_token"];
+    if(isset($_REQUEST["access_token"])){
+       $_SESSION["access_token"] = $_REQUEST["access_token"];
     }
 }
 else{
