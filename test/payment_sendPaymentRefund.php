c2b693220baf1d30cfaaa1d56fd666283b30506b
acasado <acasado@opensistemas.com>
2016-05-25 16:22:16 +0200

    TEST Payment Refund with OTP

--- test/payment_sendPaymentRefund.php
+++ test/payment_sendPaymentRefund.php
@@ -24,7 +24,15 @@ $params = array(
 <br />
 <pre>
 <?php
-if ( isset($_REQUEST['transaction_id']) && isset($_SESSION['access_token']) ):
+if( isset($_SESSION['otp_token']) && isset($_SESSION['info_refund']) && isset($_REQUEST['ticket']) && isset($_REQUEST['result']) && $_REQUEST['result'] == 'OK' ){
+    //RECOVERY INFO REFUND
+    $info_refund = unserialize($_SESSION["info_refund"]);
+    //RECOVERY OTP TOKEN
+    $params['token'] = $_SESSION['otp_token'];
+    $NimbleApi = new NimbleAPI($params);
+    $response = NimbleAPIPayments::sendPaymentRefund($NimbleApi, $info_refund['transaction_id'], $info_refund['amount']);
+    var_dump($response);
+}else if ( isset($_REQUEST['transaction_id']) && isset($_SESSION['access_token']) ){
     /* High Level call */
     $amount = array ("amount" => 300,
                      "concept"=> "Shoes",
@@ -33,8 +41,28 @@ if ( isset($_REQUEST['transaction_id']) && isset($_SESSION['access_token']) ):
     $transaction_id = $_REQUEST['transaction_id'];
     $params['token'] = $_SESSION['access_token'];
     $NimbleApi = new NimbleAPI($params);
-    $response2 = NimbleAPIPayments::sendPaymentRefund($NimbleApi, $transaction_id, $amount);
-    var_dump($response2);
-endif;
+    $response = NimbleAPIPayments::sendPaymentRefund($NimbleApi, $transaction_id, $amount);
+    var_dump($response);
+    
+    //BACK URL
+    $pre_url = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
+    $back_url = $pre_url . $_SERVER['SERVER_NAME'] . $_SERVER['PATH_INFO'];
+    var_dump($back_url);
+    //TICKET
+    $ticket = $response['data']['ticket'];
+    //TOKEN OTP
+    $_SESSION["otp_token"] = $response['data']['token'];
+    //REFUND INFO
+    $info_refund = array(
+        'amount' => $amount,
+        'transaction_id' => $transaction_id
+    );
+    $_SESSION["info_refund"] = serialize($info_refund);
+    
+    
+    $url_otp = NimbleAPI::getOTPUrl($ticket, $back_url);
+    echo "<a href='{$url_otp}'>LINK OTP</a>";
+    
+}
 ?>
 </pre>
